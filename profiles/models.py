dcd0425ebc2562c4a5aef973347f33d063f9b32f
Anna Makarudze <amakarudze@gmail.com>
2017-05-09 22:25:48 +0200

    Added help text for profile model

--- profiles/models.py
+++ profiles/models.py
@@ -6,11 +6,11 @@ from libgravatar import Gravatar
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(max_length=500, blank=True)
-    location = models.CharField(max_length=30, blank=True)
-    birth_date = models.DateField(null=True, blank=True)
-    contact_number = models.CharField(max_length=16, null=True, blank=True)
-    home_page = models.CharField(max_length=255)
+    bio = models.TextField(max_length=500, help_text="Tell us a bit about yourself and your work with Python", blank=True)
+    location = models.CharField(max_length=30, help_text="City, Country", blank=True)
+    birth_date = models.DateField(null=True, help_text="Please enter your date of birth in the format YYYY-MM-DD", blank=True)
+    contact_number = models.CharField(max_length=16, help_text="Please include your country code.", null=True, blank=True)
+    home_page = models.CharField(max_length=255, help_text="Your website/blog URL.")
     twitter_handle = models.CharField(max_length=15)
     github_username = models.CharField(max_length=32)
 
