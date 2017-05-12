856d9886d2efaf30013c51a289b0d2e64a0a90a5
Anna Makarudze <amakarudze@gmail.com>
2017-05-13 00:34:25 +0200

    Modified contact form layout

--- website/forms.py
+++ website/forms.py
@@ -19,16 +19,15 @@ class ContactForm(forms.ModelForm):
         self.helper = FormHelper()
         self.helper.form_id = 'id-Crispy_ContactForm'
         self.helper.form_class = 'form-horizontal'
-        self.helper.add_input(Submit('send', 'Send Message'))
         # Moving field labels into placeholders
         layout = self.helper.layout = Layout()
         for field_name, field in self.fields.items():
             layout.append(Field(field_name, placeholder=field.label))
         self.helper.form_show_labels = False
-
+        self.helper.add_input(Submit('send', 'Send Message'))
 
 class SubscribeForm(forms.ModelForm):
-    
+
     class Meta:
         model = Subscription
         fields = ('email',)
