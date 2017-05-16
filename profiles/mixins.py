6e8ba882d9172ea0b6525647fc17f8aa6b8f9576
hamub <hbutau35@gmail.com>
2017-05-16 15:49:04 +0200

    added mixin to prevent any loged in user from editing any profile

---
+++ profiles/mixins.py
@@ -0,0 +1,15 @@
+from django.core.exceptions import PermissionDenied
+
+
+class EditOwnProfileMixin():
+    def get_object(self, *args, **kwargs):
+        obj = super(EditOwnProfileMixin, self).get_object(*args, **kwargs)
+        self.check_permission(obj)
+        return obj
+    def check_permission(self, obj):
+        if hasattr(obj, 'user'):
+            obj = obj.user
+            if obj == self.request.user:
+                return
+            else:
+                raise PermissionDenied()
