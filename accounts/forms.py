266c142a1890ab38a035a3532b86849b920fa421
hamub <hbutau35@gmail.com>
2017-06-28 23:40:52 +0200

    merged changes from master

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,10 +6,9 @@ from crispy_forms.layout import Layout, Submit
 
 
 class RegistrationForm(UserCreationForm):
-    
+
     email = forms.CharField(max_length=75, required=True)
 
-    
     def __init__(self, *args, **kwargs):
         super(RegistrationForm, self).__init__(*args, **kwargs)
 
