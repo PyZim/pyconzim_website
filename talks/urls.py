6a6b4b85201a53202ab8ada3fb9d7e85115437da
Anna Makarudze <amakarudze@gmail.com>
2018-02-10 12:38:46 +0200

    Removed talk list and accepted talks

--- talks/urls.py
+++ talks/urls.py
@@ -13,11 +13,11 @@ router.register(r'talks', TalkViewsSets)
 
 urlpatterns = [
     url(r'^submit_talk', login_required(views.submit_talk), name='submit_talk'),
-    url(r'^talk_list', login_required(views.TalkList.as_view()), name='talk_list'),
+    # url(r'^talk_list', login_required(views.TalkList.as_view()), name='talk_list'),
     url(r'^edit_talk/(?P<pk>\d+)/$', login_required(views.TalkView.as_view()), name='edit_talk'),
     url(r'^submitted', login_required(views.SuccessView.as_view()), name='submitted'),
     url(r'^accepted_talks', views.AcceptedTalksView.as_view(), name='accepted_talks'),
-    url(r'^talk_details/(?P<pk>\d+)/$', views.TalkDetailView.as_view(), name='talk_details'),
+    # url(r'^talk_details/(?P<pk>\d+)/$', views.TalkDetailView.as_view(), name='talk_details'),
     ]
 
 urlpatterns += router.urls
